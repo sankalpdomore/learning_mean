3ad5527dda30faa5fa5d1551df2567c7e799987e
Sankalp Sinha <sankalpsinha@Sankalps-iMac.local>
2021-02-07 21:04:14 +0530

    first time code push

---
+++ server.js
@@ -0,0 +1,17 @@
+// start making comments
+// start making comments
+// start making comments
+// start making comments
+// start making comments
+
+
+
+const http = require('http');
+const app = require('./backend/app');
+
+const port = process.env.PORT || 3000;
+
+app.set('port', port);
+const server = http.createServer(app);
+
+server.listen(port);
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   { title: "First Post", content: "This is a first post"},
+  //   { title: "2nd Post", content: "This is a first post"},
+  //   { title: "3rd Post", content: "This is a first post"}
+  // ];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit(){
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+
+}
